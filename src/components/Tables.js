a9319ede55b03c8ad753b132545903db2963d0db
Joose Helle <joose.helle@helsinki.fi>
2018-03-30 13:21:16 +0300

    Start using styled-components

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,23 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const TablesContainer = styled.div`
+  width: 220px;
+  border-radius: 7px;
+  margin: 5px;
+  padding: 4px;
+  background: white;
+  opacity: 0.9;
+  border: 2px solid black;
+  font-size: 14px;
+  display: ${props => props.display}
+`
+
+const StyledTable = styled.div`
+  padding: 5px;
+  border-radius: 7px; 
+  &:hover { background: #e8e8e8; }
+`
+const Paragraph = styled.div`
+  padding: 0px;
+`
 
 const Tables = ({ tables }) => {
-
   let display = tables.length === 0 ? 'none' : ''
 
   return (
-    <div style={{ background: 'white', width: 250, border: '1px solid black', display: display }}>
+    <TablesContainer display={display}>
       {tables.map(table => <Table key={table.properties.title} table={table} />)}
-    </div>
+    </TablesContainer>
   )
 }
 
 const Table = ({ table }) => {
   return (
-    <div>
+    <StyledTable>
       <b>{table.properties.title}</b> {table.properties.likes} likes
-      <p> {table.properties.description} </p>
-    </div>
+      <Paragraph> {table.properties.description} </Paragraph>
+    </StyledTable>
   )
 }
 
