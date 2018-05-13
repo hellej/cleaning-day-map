1c8364c693143905e62caef1b96e8d11bbbe88b4
Joose Helle <joose.helle@helsinki.fi>
2018-05-13 17:44:13 +0300

    Adjust notification style

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -12,7 +12,7 @@ font-size: 14px;
 font-weight: 300;
 letter-spacing: 1.5px;
 //margin: 3px 0px 3px 10px;
-padding: 12px 20px;
+padding: 18px 23px;
 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 ${props => props.type === 'info' && css`
   background: rgba(0,0,0,0.8);
